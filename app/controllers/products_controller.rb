a4a6b0a00dc958f68806ee33e802d6139dfb3622
sakaisatoru310 <tuba310@gmail.com>
2020-04-02 17:20:59 +0900

    イメージ以外編集、削除可能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,9 +8,22 @@ class ProductsController < ApplicationController
   def show
   end
   def edit
+    @product = Product.find(params[:id])
+  end
+  def buy #商品確認画面です
   end
   def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to root_path
   end
   def update
+    @product = Product.find(params[:id])
+    @product.update!(product_params)
+    redirect_to product_path(@product.id)
+  end
+  private
+  def product_params
+    params.require(:product).permit(:name, :details, :categories, :price, :condition, :exhibition, :shippingarea, :shippingdate)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_many :images
   has_many :main_categories
   has_many :comments
