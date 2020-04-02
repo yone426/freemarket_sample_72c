327f21407ec007a44d596bb4c7bd9cb791d67106
yone426 <yone14671467@yahoo.co.jp>
2020-04-02 17:20:32 +0900

    image以外投稿可能！

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,36 @@
 class ProductsController < ApplicationController
+
   def index
+    @products = Product.all
   end
+
   def new
+    @product = Product.new
   end
+
   def create
+    @product = Product.create(product_params)
+    redirect_to root_path
   end
+
   def show
+
   end
+
   def edit
+
   end
+
   def destroy
+
   end
+
   def update
+
   end
+
+  private
+    def product_params
+      params.require(:product).permit(:details, :name, :categories, :price, :condition, :exhibition, :shippingarea, :shippingdate)
+    end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ApplicationRecord
-  belongs_to :user
+  belongs_to :user ,optional: true
   has_many :images
   has_many :main_categories
   has_many :comments
   has_many :likes
+  validates :name, :price, :details, :categories, :condition, :exhibition, :shippingarea, :shippingdate, presence: true
 end
