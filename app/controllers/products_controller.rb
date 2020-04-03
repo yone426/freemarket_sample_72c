80c0a74c2a41e7291f4a1ee5e950a500dc3d7edb
kinoshitahiroki <knsthrk@gmail.com>
2020-04-03 17:45:10 +0900

    トップページの画像表示

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,11 @@
 class ProductsController < ApplicationController
   def index
+    @new_products = Product.where(status: 0).order("created_at DESC").page(params[:page]).per(3)
+    @pickup_products = Product.where(categories: '猫', status: 0).order("created_at DESC").page(params[:page]).per(3)
+    
+    @products = Product.includes(:images)
+    @images = Image.find_by(product_id: 1)
+    
   end
   def new
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_103949) do
+ActiveRecord::Schema.define(version: 2020_04_02_082040) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2020_04_01_103949) do
     t.string "shippingdate", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
