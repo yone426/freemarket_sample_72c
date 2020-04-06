b8d768db9836d372ca087f880cf26a14e4e900da
naoyatsuchida <naoyatsuchida9@gmail.com>
2020-04-06 14:15:52 +0900

    スクロール機能

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,3 +1,17 @@
-$(document).ready(function($){
-  $("categorys_content__${id}")
-})
+
+
+$(function(){
+  $('a[href^="#"]').click(function(){
+    //スクロールのスピード
+    var speed = 300;
+    //リンク元を取得
+    var href= $(this).attr("href");
+    //リンク先を取得
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    //リンク先までの距離を取得
+    var position = target.offset().top;
+    //スムーススクロール
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
