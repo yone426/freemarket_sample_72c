7ad3e07624340e276a6bf82045edf5396cf53757
naoyatsuchida <naoyatsuchida9@gmail.com>
2020-04-07 17:52:28 +0900

    fdsaf

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
 
   before_action :set_category, only: [:new]
-  before_action :set_product, except: [:index, :new, :create]
+  # before_action :set_product, except: [:index, :new, :create]
 
 
   def index
