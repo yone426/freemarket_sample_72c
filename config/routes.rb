41618183a089cec01acd160c23628250a9c0fb69
sakaisatoru310 <tuba310@gmail.com>
2020-04-07 13:03:06 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,24 @@
 Rails.application.routes.draw do
-  
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+    delete 'destroy_user_session', to: 'users/sessions#destroy'
+
+  end
+ 
   root to: "products#index"
   resources :products do
     collection do
       get "buy"
     end
   end
+  resources :images
+  
+
   resources :users do
     collection do
       get "rogout"
