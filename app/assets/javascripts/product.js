6648a1196f7437548b94e3f4404e9d8c4a435040
naoyatsuchida <naoyatsuchida9@gmail.com>
2020-04-07 17:21:20 +0900

    修正

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -80,7 +80,7 @@ $('#parent-form').on('change', function(){
     
     let opt1 = data.map(x=> `<option value="${x.id}">${x.name}</option>`)
       let opt = opt1.join('');
-      console.log(opt);
+      
       let tako = `<option value="">選択してください</option>
       ${opt}`;
       let h = `<select name="" id="child-form" selected>${tako} </select>`; //colection_selectのとこ
@@ -121,7 +121,7 @@ $(document).on('change','#child-form', function(){
         
         let opt1 = data.map(x=> `<option value="${x.id}">${x.name}</option>`)
         let opt = opt1.join('');
-        console.log(opt);
+  
         let tako = `<option value="">選択してください</option>
         ${opt}`;
         let h = `<select name="product[category_id]" id="grandchild-form" selected>${tako} </select>`; //colection_selectのとこ
