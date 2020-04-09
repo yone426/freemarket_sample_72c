8bdb31b90647f82d58d3cad5e2992083d46d25a0
naoyatsuchida <naoyatsuchida9@gmail.com>
2020-04-09 17:26:29 +0900

    マイページ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
  #マイページ関連
   def show
     @user = User.find(params[:id])
-    @products = @user.products
+    @products = @user.products.order("created_at DESC")
 
   end
 
