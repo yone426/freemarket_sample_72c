ebab69bc7df0870f1148e8e83ae02c094dbad42f
naoyatsuchida <naoyatsuchida9@gmail.com>
2020-04-10 12:43:34 +0900

    修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,10 +4,11 @@ class Address < ApplicationRecord
   belongs_to_active_hash :prefecture
   
  
-  
+  ZENKAKU=/\A[ぁ-んァ-ン一-龥]/
   
   validates :zipcode, :prefecture_id ,:municipalities,:address, presence: true
   validates :zipcode,numericality: :only_integer
-  
   validates :address,numericality: :only_integer
+  validates :municipalities,format: {with:ZENKAKU}
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_one :card
 
   KATAKANA = /\A[ァ-ヶー－]+\z/
-  FULLNAME =/\A[一-龥ぁ-ん]/
+  FULLNAME =/\A[ぁ-んァ-ン一-龥]/
   validates :email,:nickname,:fullname,:fullname_katakana,:phone_number,:birthday, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, uniqueness: true,format: { with: VALID_EMAIL_REGEX }
